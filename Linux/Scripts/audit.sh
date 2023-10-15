530e407848c141ae99777b5ffb87aaaf8257fdf1
phishfish <lucialwang@gmail.com>
2023-10-15 17:31:56 -0400

    More complex auditing

--- Linux/Scripts/audit.sh
+++ Linux/Scripts/audit.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+OUT_FILE=$1
+CURR_USER=$(whoami)
+
 main()
 {
-	echo "Beginning to audit users"
-	sudo cat /etc/passwd
+	echo "Auditing users: "
+	system_accounts=$(cut -d: -f1,3 /etc/passwd | awk -F: '$2 < 1000 {print $1}')
+	echo $CURR_USER
+	echo "$system_accounts"
+	echo $CURR_USER >> $OUT_FILE
+	echo "$system_accounts" >> $OUT_FILE
 
-	echo "Beginning to audit groups"
-	sudo cat /etc/group
+	printf "\nAuditing groups:\n"
+	system_groups=$(cut -d: -f1,3 /etc/group | awk -F: '$2 < 1000 {print $1}')
+	echo "$system_groups"
+	printf "\n" >> $OUT_FILE
+	echo "$system_groups" >> $OUT_FILE
 		
 }
 
