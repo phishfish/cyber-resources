7244b80374a2cb89cc3daf398f163126f1f8c447
phishfish <lucialwang@gmail.com>
2023-11-01 13:59:40 -0400

    Even more error checking

--- Linux/Scripts/firewall_backup.sh
+++ Linux/Scripts/firewall_backup.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
 
-# create_copy()
-# {
+OPTION="$1"
+FILE="$2"
 
-# }
+create_backup()
+{
+    return 0
+}
+
+restore_backup()
+{
+    return 0
+}
 
 main()
 {
     result=$(sudo ufw status)
     if [ "${result}" = "Status: active\n" ]; then
-        echo "ufw not enabled (have you run "sudo ufw enable?")"
+        echo "ufw not enabled (have you run \"sudo ufw enable?\")"
         exit 1
     fi
 
-    echo "Printing current rules..."
-    
+    if [ "${OPTION}" = "backup" ]; then
+        echo "Backing up current rules..."
+        create_backup
+        if [ "$?" -eq 0 ]; then
+            echo "Backup completed"
+        else
+            echo "Backup failed"
+        fi
+    elif [ "${OPTION}" = "restore" ]; then
+        echo "Restoring to previous backup..."
+        if [ "$?" -eq 0 ]; then
+            echo "Backup restored"
+        else
+            echo "Restoration failed"
+        fi
+    else
+        echo "Improper usage (please run in "./firewall_backup.sh \<backup/restore\> \<directory storing backups\>" format)"
+        exit 1
+    fi
+
 } 
 
 main
