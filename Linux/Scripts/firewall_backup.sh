09c288f3cca05c2ac7b224f978964744213b2086
phishfish <lucialwang@gmail.com>
2023-11-01 16:30:03 -0400

    I should probably test this but oh well

--- Linux/Scripts/firewall_backup.sh
+++ Linux/Scripts/firewall_backup.sh
@@ -1,16 +1,83 @@
 #!/bin/bash
 
 OPTION="$1"
-FILE="$2"
+FW_CHOICE="$2"
+DIR="$3"
 
 create_backup()
 {
-    return 0
+    if [ "${FW_CHOICE}" = "ufw" ]; then
+        for file in /etc/ufw/; do
+            filename=$(basename "$file")
+            
+            # Create a backup filename by adding a timestamp
+            timestamp=$(date +"%Y%m%d%H%M%S")
+            backup_filename="${filename%.*}_etc_backup_$timestamp.rules"
+            
+            # Copy the file to the backup directory
+            sudo cp -r "$file" "$backup_dir$backup_filename"
+            
+            echo "Backed up $file to $backup_dir$backup_filename"
+        done
+
+        for file in /lib/ufw/; do
+            filename=$(basename "$file")
+            
+            # Create a backup filename by adding a timestamp
+            timestamp=$(date +"%Y%m%d%H%M%S")
+            backup_filename="${filename%.*}_lib_backup_$timestamp.rules"
+            
+            # Copy the file to the backup directory
+            sudo cp -r "$file" "$backup_dir$backup_filename"
+            
+            echo "Backed up $file to $backup_dir$backup_filename"
+        done
+    
+    elif [ "${FW_CHOICE}" = "iptables" ]; then
+        sudo iptables-save > "$FILE/iptables_backup_$(date +'%Y%m%d%H%M%S').rules" 
+    else 
+        echo "Please enter ufw or iptable rules to back up"
+        exit 1
+    fi
+    return $?
 }
 
 restore_backup()
 {
-    return 0
+    # Restore UFW rules from a backup file
+    if [ "${FW_CHOICE}" = "ufw" ]; then
+        for backup_file in "$backup_dir"/*_etc_backup_*.rules; do
+            if [ -f "$backup_file" ]; then
+                # Extract the original filename from the backup filename
+                original_filename=$(basename "$backup_file" | sed 's/_etc_backup_.*\.rules/.rules')
+
+                # Restore the file to /etc/ufw/
+                sudo cp -r "$backup_file" "/etc/ufw/$original_filename"
+
+                echo "Restored $backup_file to /etc/ufw/$original_filename"
+            fi
+        done
+
+        for backup_file in "$backup_dir"/*_lib_backup_*.rules; do
+            if [ -f "$backup_file" ]; then
+                # Extract the original filename from the backup filename
+                original_filename=$(basename "$backup_file" | sed 's/_lib_backup_.*\.rules/.rules')
+
+                # Restore the file to /lib/ufw/
+                sudo cp -r "$backup_file" "/lib/ufw/$original_filename"
+
+                echo "Restored $backup_file to /lib/ufw/$original_filename"
+            fi
+        done
+    elif [ "${FW_CHOICE}" = "iptables" ]; then
+        result=$(find "${DIR}" -name "iptables_backup_*.rules")
+        sudo iptables-restore "$filename"
+    else
+        echo "Please choose to restore ufw or iptable rules" 
+        exit 1
+    fi
+
+    return $?
 }
 
 main()
@@ -37,10 +104,11 @@ main()
             echo "Restoration failed"
         fi
     else
-        echo "Improper usage (please run in "./firewall_backup.sh \<backup/restore\> \<directory storing backups\>" format)"
+        echo "Improper usage (please run in "./firewall_backup.sh \<backup/restore\> \<ufw or iptables\> \<directory storing backups\>" format)"
         exit 1
     fi
 
+    echo "Program has finished"
 } 
 
 main
